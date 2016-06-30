acba00510e1889b581ca3a57dad1ce471d497d72
athityakumar <athityakumar@gmail.com>
2016-06-30 14:04:49 +0530

    Add auto-commit messages

--- git.rb
+++ git.rb
@@ -1,3 +1,18 @@
-system "git add ."
-system "git commit -m 'Ruby-bot git commit'"
-system "git push origin master"
+if `git status`.include? "fatal: Not a git repository"
+  `git init`
+else
+  puts "Git init - checked."  
+end  
+
+
+#system "git add --all"
+
+#system "git commit -m 'Ruby-bot git commit'"
+#system "git push origin master"
+
+out = `git remote -v`
+if out.empty?
+  puts "Empty"
+else
+  puts out
+end
