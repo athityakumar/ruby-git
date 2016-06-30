4957d2990faf29ef76e4a474afda899325e146a3
athityakumar <athityakumar@gmail.com>
2016-06-30 14:09:10 +0530

    Ruby-bot commit :  .  . 	modified:   git.rb .  .

--- git.rb
+++ git.rb
@@ -1,18 +1,41 @@
 if `git status`.include? "fatal: Not a git repository"
   `git init`
+  puts "Git init - done."
 else
   puts "Git init - checked."  
 end  
 
-
-#system "git add --all"
-
-#system "git commit -m 'Ruby-bot git commit'"
-#system "git push origin master"
-
-out = `git remote -v`
-if out.empty?
-  puts "Empty"
+if `git remote -v`.empty?
+  puts "Paste remote git URL : "
+  remote = gets.chomp
+  `git remote add origin #{remote}`
+  puts "Git remote - done."
 else
-  puts out
+  puts "Git remote - checked."
 end
+
+if `git status`.include? "Changes not staged for commit:"
+  `git add --all`
+  puts "Git add - done."
+else
+  puts "Git add - checked."  
+end  
+
+if `git status`.include? "Changes to be committed:"
+  #puts "Enter commit message : "
+  message = ""
+  if message.empty?
+    message = `git status`.split('(use "git reset HEAD <file>..." to unstage)')[1].gsub("\n"," . ")
+  end  
+  `git commit -m "Ruby-bot commit : #{message}"`
+  puts "Git commit - done."  
+else
+  puts "Git commit - checked."  
+end  
+
+if `git status`.include? "nothing to commit, working directory clean"
+  `git push origin master`
+  puts "Git push - done."  
+else
+  puts "Git push - checked."  
+end  
